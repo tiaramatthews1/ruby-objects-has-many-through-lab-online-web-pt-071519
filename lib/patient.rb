6c93f140c0a6cfdfb8ae0aa257940dc969d2176c
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-09-11 17:02:19 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,7 +13,8 @@ class Patient
   end
 
   def appointments
-    Appointment.all.select {|appointment| appointment.patient == self}
+    Appointment.all.select do |appointment| appointment.patient == self
+    end
   end
 
   def doctors
