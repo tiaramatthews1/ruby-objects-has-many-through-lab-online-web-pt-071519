45666e1429ee7c8fb00cb3cf2d9bccd8857a6e92
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-09-11 17:36:24 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 class Patient
 
   attr_accessor :name, :appointment, :doctor
@@ -15,6 +15,7 @@ class Patient
 
   def appointments
     Appointment.all.select do |appointment| 
+      binding.pry
       appointment.patient == self
     end
   end
